beb4dde512a9ae91fb0b5f45c21118d68b713924
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-01-29 16:33:48 +0300

    Недоделанный адаптив

--- js/Menu.js
+++ js/Menu.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//     var $toggleButton = $('.toggle-button');
-//     $toggleButton.on('click', function() {
-//       $(this).toggleClass('button-open');
-//     });
-//   });
 
 $(document).ready(function() {
   var $toggleButton = $('.toggle-button'),
--- js/SwitchTheme.js
+++ js/SwitchTheme.js
@@ -3,21 +3,21 @@
       link = document.createElement('link');
   link.rel = 'stylesheet';
   if (localStorage.getItem('themeStyle') === 'dark') {
-    link.href = '../css/StyleDarkTheme.css'; 
+    link.href = 'css/StyleDarkTheme.css'; 
     document.getElementById('switch-1').setAttribute('checked', true); 
   }
   else {
-    link.href = '../css/StyleLiteTheme.css'; 
+    link.href = 'css/StyleLiteTheme.css'; 
   }
   head.appendChild(link);
   document.getElementById('switch-1').addEventListener('change', ev => {
     let btn = ev.target;
     if (btn.checked) {
-      link.href = '../css/StyleDarkTheme.css'; 
+      link.href = 'css/StyleDarkTheme.css'; 
       localStorage.setItem('themeStyle', 'dark'); 
     }
     else {
-      link.href = '../css/StyleLiteTheme.css'; 
+      link.href = 'css/StyleLiteTheme.css'; 
       localStorage.setItem('themeStyle', 'light'); 
     }
   });
